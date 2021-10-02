df468073e1d2f8969c6d253ee97fe8d4d194301d
Hjgaming <69000638+Hjgaming@users.noreply.github.com>
2021-10-02 16:23:03 +0530

    Add files via upload

---
+++ handlers/events.js
@@ -0,0 +1,24 @@
+const ascii = require("ascii-table");
+
+const { readdirSync } = require("fs");
+
+module.exports = (client) => {
+  const eventFiles = readdirSync("./events/").filter((file) =>
+    file.endsWith(".js")
+  );
+
+  eventFiles.forEach((file) => {
+    const event = require(`../events/${file}`);
+    if (!event.execute)
+      throw new TypeError(
+        `[ERROR]: execute function is required for events! (${file})`
+      );
+
+    if (!event.name)
+      throw new TypeError(`[ERROR]: name is required for events! (${file})`);
+
+    client.on(event.name, event.execute.bind(null, client));
+
+    delete require.cache[require.resolve(`../events/${file}`)];
+  });
+};
